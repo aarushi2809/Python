9d9255075f34b2394f4f5129a38a802904619771
Aarushi Bhushit Vaishnav <aarushi2892007@gmail.com>
2026-01-20 12:51:11 +0530

    Add files via upload

---
+++ 25bbt017.py
@@ -0,0 +1,55 @@
+##a=int(input("Enter number 1:"))
+##b=int(input("Enter number 2:"))
+##if a>b:
+##    print("ä is greater than b")
+##else:
+##    print("b is greater than a")
+
+##a=int(input("Enter number 1:"))
+##b=int(input("Enter number 2:"))
+##c=int(input("Ënter number 3:"))
+##if a>b and a>c:
+##    print("a is largest")
+##elif b>a and b>c:
+##    print("b is the largest")
+##else:
+##    print("c is the largest")
+
+##a=float(input("Enter a digit:"))
+##if a%2==0:
+##    print(" it is an even number")
+##else:
+##    print("it is an odd number")
+
+##a=float(input("Enter a digit:"))
+##if a%10==0:
+##    print(" it is divisible by 10")
+##else:
+##    print("it is not divisible by 10")
+
+##a=int(input("Enter age:"))
+##if a>=18:
+##    print("major")
+##else:
+##    print("minor")
+
+##a=input("Enter a number:")
+##print("number of digits=",len(a))
+
+a=int(input("Enter a year:"))
+if (a%4==0 and a%400==0) or (a%100!=0):
+    print("it is leap year")
+else:
+    print("it is not a leap year")
+
+
+
+
+
+
+
+
+
+
+
+
