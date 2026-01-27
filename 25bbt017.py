48041f4795f59f8adf9270acfc192e7a6e63c43e
Aarushi Bhushit Vaishnav <aarushi2892007@gmail.com>
2026-01-27 12:46:02 +0530

    Add files via upload

--- 25bbt017.py
+++ 25bbt017.py
@@ -1,55 +1,22 @@
-##a=int(input("Enter number 1:"))
-##b=int(input("Enter number 2:"))
-##if a>b:
-##    print("ä is greater than b")
+x=5
+
+#y=3
+
+z=2
+
+##if(y-z >5-y):
+##
+##	y=y+3
+##print(y)
+
+
+if(x-z):
+
+	#z=z+2
+                print(z)
+
 ##else:
-##    print("b is greater than a")
-
-##a=int(input("Enter number 1:"))
-##b=int(input("Enter number 2:"))
-##c=int(input("Ënter number 3:"))
-##if a>b and a>c:
-##    print("a is largest")
-##elif b>a and b>c:
-##    print("b is the largest")
-##else:
-##    print("c is the largest")
-
-##a=float(input("Enter a digit:"))
-##if a%2==0:
-##    print(" it is an even number")
-##else:
-##    print("it is an odd number")
-
-##a=float(input("Enter a digit:"))
-##if a%10==0:
-##    print(" it is divisible by 10")
-##else:
-##    print("it is not divisible by 10")
-
-##a=int(input("Enter age:"))
-##if a>=18:
-##    print("major")
-##else:
-##    print("minor")
-
-##a=input("Enter a number:")
-##print("number of digits=",len(a))
-
-a=int(input("Enter a year:"))
-if (a%4==0 and a%400==0) or (a%100!=0):
-    print("it is leap year")
-else:
-    print("it is not a leap year")
-
-
-
-
-
-
-
-
-
-
-
-
+##
+##	x=x-1
+##
+##print(x+y*2-z)
